6bf0c9b4a8089fc2330d07f1d5842bd0b3e53ce6
Gianni Carlo <gcarlo89@hotmail.com>
2018-04-06 15:14:16 -0500

    Carthage ready

---
+++ UIImageView-Letters/UIImageView_Letters.h
@@ -0,0 +1,19 @@
+//
+//  UIImageView_Letters.h
+//  UIImageView-Letters
+//
+//  Created by Gianni Carlo on 4/6/18.
+//  Copyright © 2018 Criptext Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageView_Letters.
+FOUNDATION_EXPORT double UIImageView_LettersVersionNumber;
+
+//! Project version string for UIImageView_Letters.
+FOUNDATION_EXPORT const unsigned char UIImageView_LettersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageView_Letters/PublicHeader.h>
+
+
